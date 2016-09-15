f7a590d2f7ba8892da97adc0e2ba010065124bf8
Joshua Chung <jchung13@vt.edu>
2016-09-14 20:29:02 -0400

    added sequence state machine

--- firmware/src/int_wifly.c
+++ firmware/src/int_wifly.c
@@ -13,7 +13,17 @@ unsigned send_buffer_pos;
 CharBuffer recv_buffer;
 unsigned recv_buffer_pos;
 
+unsigned sequence_counter;
+const char sequence_buffer[] = {128, 37, 35, 36};
+uint8_t crc;
+typedef enum {INIT, CRC, LENGTH, MSG} SEQUENCE_STATE;
+SEQUENCE_STATE sequence_state;
+
+void wifly_int_crc(uint8_t byte);
 void wifly_int_init() {
+    sequence_state = CRC;
+    crc = 0;
+    sequence_counter = 0;
     send_buffer_pos = 0;
     send_buffer.length = 0;
     recv_buffer_pos = 0;
@@ -49,6 +59,37 @@ void wifly_int_acknowledge() {
 }
 
 void wifly_int_recv_byte(char byte) {
+    //check if the sequence bytes match...should count up to 4
+    if(sequence_buffer[sequence_counter] == byte){
+        sequence_counter++;
+    }
+    else{
+        sequence_counter = 0;
+        sequence_state = INIT;
+    }
+    if(sequence_counter == 4){
+        sequence_state = CRC;
+        sequence_counter = 0;
+    }
+    
+    switch(sequence_state){
+        case INIT:
+            sequence_state = INIT;
+            break;
+        case CRC:
+            crc = 0; 
+            wifly_int_crc(byte);
+            sequence_state = LENGTH;
+            break;
+        case LENGTH:            
+            
+            sequence_state = MSG;
+            break;
+        case MSG:
+            
+            break;
+    }
+    //if all the sequenced match then the counter should be 4
     if (recv_buffer.length == recv_buffer_pos) {
         recv_buffer.length = (unsigned)byte + 1;
         recv_buffer_pos = 0;
@@ -58,4 +99,15 @@ void wifly_int_recv_byte(char byte) {
             network_recv_add_buffer_from_isr(&recv_buffer);
         }
     }
+}
+
+void wifly_int_crc(uint8_t byte){
+	uint8_t i;
+    crc ^= (byte << 8);
+	for(i = 8; i; i--) {
+		if (crc & 0x8000)
+			crc ^= (0x1070 << 3);
+		crc <<= 1;
+	}
+	
 }
